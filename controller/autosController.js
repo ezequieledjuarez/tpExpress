94473f867a5993a6bc4f6c11c27574b506f77769
EzzeGuason <ezze.juarezz@gmail.com>
2020-07-25 23:50:27 -0300

    Controller autos finalizado?

--- controller/autosController.js
+++ controller/autosController.js
@@ -6,10 +6,30 @@ module.exports = autosControllers = {
         db.forEach(concesionaria => {
             res.write(concesionaria.sucursal + '\n\n')
             concesionaria.autos.forEach(auto => {
-                res.write(auto.marca + ' ' + auto.modelo + '\n')
+                res.write(auto.marca + ' ' + auto.modelo + ' ' + auto.anio + '\n')
+
             })
+            res.write('\n')
         })
         res.end()
     },
+    dato: function(req, res) {
+        let marcaId = req.params.id
+        let datoId = req.params.dato
+        db.forEach(concesionaria => {
+            concesionaria.autos.forEach(auto => {
+                if (datoId.toLowerCase() == auto.color.toLowerCase() || marcaId.toLowerCase() == auto.marca) {
+                    res.write(auto.marca + ' ' +
+                        auto.modelo + ' ' +
+                        auto.anio + ' ' + auto.color + '\n\n')
+                } else if (datoId == auto.anio || marcaId.toLowerCase() == auto.marca) {
+                    res.write(auto.marca + ' ' +
+                        auto.modelo + ' ' +
+                        auto.anio + ' ' + auto.color + '\n\n')
+                }
+            })
+        })
+        res.end('Esos son los modelos que tenemos disponibles')
 
+    }
 }
--- routes/autosRoutes.js
+++ routes/autosRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const autoController = require('../controller/autosController')
+const marcaController = require('../controller/marcasController')
 const router = express.Router()
 
 router.get('/', autoController.index)
+router.get('/:id', marcasController.marca)
+router.get('/:id/:dato', autoController.dato)
+
 module.exports = router
